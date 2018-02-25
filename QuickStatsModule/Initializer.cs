5cccc26cc6113aae0ed520b6dd8042ccd55f3399
tjcertified <tjcertified@gmail.com>
2018-02-25 00:02:18 -0600

    Added code to auto-initialize module when added to site's bin folder.
    Added Key Signing in an attempt to load module for all sites (didn't work)

---
+++ QuickStatsModule/Initializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Web.Administration;
+
+namespace QuickStatsModule
+{
+    /// <summary>
+    /// From http://blog.davidebbo.com/2011/02/register-your-http-modules-at-runtime.html,
+    /// This allows you to drop in this dll to an IIS /bin folder and have it "just work"
+    /// for all sites, as long as you add the assembly attribute noted there.
+    /// </summary>
+    public class Initializer
+    {
+        public static void Start()
+        {
+            //Register the QuickStatsModule
+            HttpApplication.RegisterModule(typeof(QuickStatsModule));
+        }
+    }
+}
