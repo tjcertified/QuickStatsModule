cff7c97daaed027c7b69e7eba398f2635afd7bc7
tjcertified <tjcertified@gmail.com>
2018-02-26 21:44:41 -0600

    changed keys

--- QSMInstaller/Program.cs
+++ QSMInstaller/Program.cs
@@ -24,9 +24,23 @@ namespace QSMInstaller
                 var element = collection.CreateElement();
                 element.Attributes["name"].Value = "QuickStatsModule";
                 element.Attributes["type"].Value =
-                    $"{nameof(QuickStatsModule)}.{nameof(QuickStatsModule.QuickStatsModule)}, {nameof(QuickStatsModule)}, Version=1.0.0.0, Culture=neutral, PublicKeyToken=0d1283041f310768";
+                    $"{nameof(QuickStatsModule)}.{nameof(QuickStatsModule.QuickStatsModule)}, {nameof(QuickStatsModule)}, Version=1.0.0.0, Culture=neutral, PublicKeyToken=fb52c173020a0e2f";
                 Console.WriteLine($"Module being installed is: {element.Attributes["name"].Value}");
                 Console.WriteLine($"Module install details: {element.Attributes["type"].Value}");
+
+                ConfigurationElement redundantElement = null;
+                foreach(var el in collection)
+                {
+                    if (el.Attributes["name"].Value.Equals(element.Attributes["name"].Value))
+                    {
+                        redundantElement = el;
+                        break;
+                    }
+                }
+                if (redundantElement != null)
+                {
+                    collection.Remove(redundantElement);
+                }
                 collection.Add(element);
                 serverManager.CommitChanges();
                 Console.WriteLine("'QuickStatsModule' successfully installed to the GAC and IIS.");
